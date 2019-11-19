bd57ffa62daad5a8c0cacba97788230acde2d7f2
nevroje <joakim.berglind@gmail.com>
2019-11-19 14:53:11 +0100

    Minify JS, CSS and HTML in prod extraced CSS into own file in production added terserJS back to minify js in production minified html in production added important comment

--- webpack.common.js
+++ webpack.common.js
@@ -1,5 +1,4 @@
 const path = require("path");
-var HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   // devtool: "none",
@@ -7,22 +6,8 @@ module.exports = {
     main: "./src/index.js",
     vendor: "./src/vendor.js"
   },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: "./src/template.html"
-    })
-  ],
   module: {
     rules: [
-      {
-        test: /\.scss$/,
-        use: [
-          // These loaders needs to be reversed in a correct order to work (see comments 1,2,3 below);
-          "style-loader", //3 injects styles into DOM;
-          "css-loader", //2 Turns css into commonjs;
-          "sass-loader" //1 Turns sass into css;
-        ]
-      },
       {
         test: /\.html$/,
         use: ["html-loader"]
--- webpack.dev.js
+++ webpack.dev.js
@@ -1,6 +1,7 @@
 const path = require("path");
 const common = require("./webpack.common");
 const merge = require("webpack-merge");
+var HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = merge(common, {
   // devtool: "none",
@@ -8,5 +9,23 @@ module.exports = merge(common, {
   output: {
     filename: "[name].bundle.js",
     path: path.resolve(__dirname, "dist")
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/template.html"
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [
+          // These loaders needs to be reversed in a correct order to work (see comments 1,2,3 below);
+          "style-loader", //3 injects styles into DOM;
+          "css-loader", //2 Turns css into commonjs;
+          "sass-loader" //1 Turns sass into css;
+        ]
+      }
+    ]
   }
 });
