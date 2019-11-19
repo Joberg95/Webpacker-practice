87021554cc90d11e5fe4b2ba0636926a17442af5
nevroje <joakim.berglind@gmail.com>
2019-11-19 13:41:58 +0100

    Cache busting and htmlwebpackPlugin Configured webpack to use contentHash in bundle file name installed HtmlWebpackPlugin to generate new index.html file from template

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import { run } from "./app/app";
 import "./main.scss";
 import { ComponentService } from "./app/component.service";
 import { AlertService } from "./app/alert.service";
-
 const alertService = new AlertService();
 const componentService = new ComponentService();
 run(alertService, componentService);
--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,28 @@
 const path = require("path");
+var HtmlWebpackPlugin = require("html-webpack-plugin");
+
 module.exports = {
   mode: "development",
   // devtool: "none",
   entry: "./src/index.js",
   output: {
-    filename: "main.js",
+    filename: "main.[contentHash].js",
     path: path.resolve(__dirname, "dist")
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/template.html"
+    })
+  ],
   module: {
     rules: [
       {
         test: /\.scss$/,
         use: [
-        // These loaders needs to be reversed in a correct order to work (see comments 1,2,3 below);
-        "style-loader",  //3 injects styles into DOM;
-        "css-loader",    //2 Turns css into commonjs;
-        "sass-loader"    //1 Turns sass into css;
+          // These loaders needs to be reversed in a correct order to work (see comments 1,2,3 below);
+          "style-loader", //3 injects styles into DOM;
+          "css-loader", //2 Turns css into commonjs;
+          "sass-loader" //1 Turns sass into css;
         ]
       }
     ]
