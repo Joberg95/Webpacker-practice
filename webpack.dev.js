f582c2da99310f997030ed2903b34b6490ac002b
nevroje <joakim.berglind@gmail.com>
2019-11-19 13:57:47 +0100

    Add prod and dev configs dev-server split webpack into 3 files installed webpack-merge updated package.json to use dev server

---
+++ webpack.dev.js
@@ -0,0 +1,12 @@
+const path = require("path");
+const common = require("./webpack.common");
+const merge = require("webpack-merge");
+
+module.exports = merge(common, {
+  // devtool: "none",
+  mode: "development",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist")
+  }
+});
