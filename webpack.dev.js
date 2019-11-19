53810bcbd8a8ef0e8bc37d698b726ee65ec9804f
nevroje <joakim.berglind@gmail.com>
2019-11-19 14:25:56 +0100

    Add entrypoint for vendor.js file, added bootstrap js configured webpacker to make 2 entry files and 2 bundles

--- webpack.dev.js
+++ webpack.dev.js
@@ -6,7 +6,7 @@ module.exports = merge(common, {
   // devtool: "none",
   mode: "development",
   output: {
-    filename: "main.js",
+    filename: "[name].bundle.js",
     path: path.resolve(__dirname, "dist")
   }
 });
